d7c029193bd0dc774e6633025965de8bf21732a4
seal0112 <seal0112@gmail.com>
2019-08-23 00:17:06 +0800

    add new route '/catalog/categories/JSON' for return category item
    1. add route '/catalog/categories/JSON' for return category item
    2. add route '/catalog/<category>/items' for todo
    3. add route '/catalog/<category>/<catalogItem>' for todo

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CatalogItem, User
@@ -9,9 +9,27 @@ app = Flask(__name__)
 engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.bind = engine
 
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 def showCatalog():
-    return 'haha'
+    return "Catalog Page"
+
+
+@app.route('/catalog/<category>/items')
+def showCatagories(category):
+    return "Catalog Page"
+
+@app.route('/catalog/<category>/<catalogItem>')
+def showCategoryItems():
+    return "Category Item"
+
+# JSON APIs to view Restaurant Information
+@app.route('/catalog/categories/JSON')
+def CategoriesJSON():
+    categories = session.query(Category).all()
+    return jsonify(category=[category.serialize for category in categories])
 
 if __name__ == '__main__':
     app.debug = True
